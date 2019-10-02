901a1507990037a8d6d3e4d4ef7234bfde8a103c
eduardo.reis <eduardo.reis@dbserver.com.br>
2019-10-02 14:04:41 -0300

    TextField DatePicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -17,6 +20,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -26,19 +30,37 @@ import model.services.SellerService;
 public class SellerFormController implements Initializable {
 
 	private Seller entity;
-	
-	private SellerService service;
-	
-	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 
-	@FXML
-	private TextField txtId;
+	private SellerService service;
+
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 
 	@FXML
 	private TextField txtName;
 
 	@FXML
+	private TextField txtId;
+
+	@FXML
+	private TextField txtEmail;
+
+	@FXML
+	private DatePicker dpBirthDate;
+
+	@FXML
+	private TextField txtBaseSalary;
+
+	@FXML
 	private Label labelErrorName;
+
+	@FXML
+	private Label labelErrorEmail;
+
+	@FXML
+	private Label labelErrorBirthDate;
+
+	@FXML
+	private Label labelErrorBaseSalary;
 
 	@FXML
 	private Button btSave;
@@ -48,55 +70,54 @@ public class SellerFormController implements Initializable {
 
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
-		if(entity == null) {
-		
+		if (entity == null) {
+
 			throw new IllegalStateException("Entity was null!");
-			
+
 		} else if (service == null) {
-			
+
 			throw new IllegalStateException("Service was null!");
-			
+
 		}
 		try {
-		entity = getFormData();
-		service.saverOrUpdate(entity);
-		notifyDataChangeListeners();
-		utils.currentState(event).close();
-		
-		}catch (DbException error) {
+			entity = getFormData();
+			service.saverOrUpdate(entity);
+			notifyDataChangeListeners();
+			utils.currentState(event).close();
+
+		} catch (DbException error) {
 			Alerts.showAlert("Error saving object", null, error.getMessage(), AlertType.ERROR);
-		}catch (ValidationException error){
+		} catch (ValidationException error) {
 			setErrorMessages(error.getErrors());
 		}
 	}
 
 	private void notifyDataChangeListeners() {
-			for (DataChangeListener listener : dataChangeListeners) {
-				listener.onDataChanged();
-			}
-		
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+
 	}
 
 	private Seller getFormData() {
 		Seller obj = new Seller();
-		
+
 		ValidationException exception = new ValidationException("Validation error!");
-		
+
 		obj.setId(utils.tryParseToTint(txtId.getText()));
-		
-		if(txtName.getText() == null || txtName.getText().trim().contentEquals("")) {
-			
+
+		if (txtName.getText() == null || txtName.getText().trim().contentEquals("")) {
+
 			exception.addError("name", "Field can't be empty!");
 		}
-		
+
 		obj.setName(txtName.getText());
-		
+
 		if (exception.getErrors().size() > 0) {
-			
+
 			throw exception;
 		}
-		
-		
+
 		return obj;
 	}
 
@@ -104,20 +125,20 @@ public class SellerFormController implements Initializable {
 
 		this.entity = entity;
 	}
-	
-	public void setSellerService(SellerService service ) {
+
+	public void setSellerService(SellerService service) {
 		this.service = service;
-		
+
 	}
-	
+
 	public void subscribeDataChangeListener(DataChangeListener listener) {
-		
+
 		dataChangeListeners.add(listener);
 	}
 
 	@FXML
 	public void onBtCancelAction(ActionEvent event) {
-		
+
 		utils.currentState(event).close();
 
 	}
@@ -130,7 +151,11 @@ public class SellerFormController implements Initializable {
 	private void initializaNodes() {
 
 		Constraints.setTextFieldInteger(txtId);
-		Constraints.setTextFieldMaxLength(txtName, 30);
+		Constraints.setTextFieldMaxLength(txtName, 70);
+		Constraints.setTextFieldDouble(txtBaseSalary);
+		Constraints.setTextFieldMaxLength(txtEmail, 60);
+		utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
+
 	}
 
 	public void updateFormData() {
@@ -139,16 +164,21 @@ public class SellerFormController implements Initializable {
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+		txtEmail.setText(entity.getEmail());
+		Locale.setDefault(Locale.US);
+		txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+		if (entity.getBirthDate() != null) {
+			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+		}
 	}
-	
+
 	private void setErrorMessages(Map<String, String> errors) {
 		Set<String> fields = errors.keySet();
-		
-		if(fields.contains("name")) {
-			
+
+		if (fields.contains("name")) {
+
 			labelErrorName.setText(errors.get("name"));
 		}
-		
-		
+
 	}
 }
